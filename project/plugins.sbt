ec582dc6fc00de32ef7b3125363a6dddbc4f60ef
jazevedo6 <jazevedo6@gatech.edu>
2019-02-25 14:46:12 -0500

    Inject constant resources from config, Prepare to be Scala Style compliant;

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,4 @@ addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.0")
 resolvers += Resolver.bintrayRepo("givers", "maven")
 
 addSbtPlugin("givers.vuefy" % "sbt-vuefy" % "4.0.0")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,14 @@
+// Settings injection object
+var formSettings = $('#formsSettings');
+var usernameRegex = new RegExp(formSettings.attr('data-name-regex'));
+var lobbyRegex = new RegExp('^[' + formSettings.attr('data-lobby-chars') + ']+$');
+var minNameLength = parseInt(formSettings.attr('data-name-min'));
+var maxNameLength = parseInt(formSettings.attr('data-name-max'));
+var lobbyLength = parseInt(formSettings.attr('data-lobby-length'));
+
 // Handle input on username field
 var usernameButton = $('#username_button');
 var usernameField = $('#username_field');
-var usernameRegex = /^[a-zA-Z0-9]+$/
 usernameField.on('input', function () {
   validateUsernameField()
 });
@@ -13,8 +20,8 @@ $(document).ready(function() {
 function validateUsernameField() {
   var username = String(usernameField.val());
   var isValidUsername = username != null
-     && username.length >= 2
-     && username.length <= 16
+     && username.length >= minNameLength
+     && username.length <= maxNameLength
      && usernameRegex.test(username);
 
   if (isValidUsername) {
@@ -27,12 +34,11 @@ function validateUsernameField() {
 // Handle input on lobby code field
 var codeField = $('#search_field');
 var codeButton = $('#search_button');
-var codeRegex = /^[BCEFGHJMPQRTVYWXbcefghjmpqrtvywx]+$/;
 codeField.on('input', function () {
   var code = String(codeField.val());
   var isValidUsername = code != null
-     && code.length === 4
-     && codeRegex.test(code);
+     && code.length === lobbyLength
+     && lobbyRegex.test(code);
 
   if (isValidUsername) {
     codeButton.prop('disabled', false);
