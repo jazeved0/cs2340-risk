d2d027ff4a62f117c385ee15c383c41513a61838
jazevedo6 <jazevedo6@gatech.edu>
2019-03-01 10:54:52 -0500

    Fix indentation

--- vue/src/util.js
+++ vue/src/util.js
@@ -4,17 +4,17 @@ export const getCookie = (cname) => {
 	const ca = decodedCookie.split(';');
 	for(var i = 0; i <ca.length; i++) {
 		const c = ca[i];
-	  while (c.charAt(0) === ' ') {
+		while (c.charAt(0) === ' ') {
 			c = c.substring(1);
-	  }
-	  if (c.indexOf(name) === 0) {
+		}
+		if (c.indexOf(name) === 0) {
 			return c.substring(name.length, c.length);
-	  }
+		}
 	}
 	return "";
 }
 
 export const pascalToUnderscore = (s) =>
 	s.replace(/(?:^|\.?)([A-Z])/g, function(x, y) {
-    return "_" + y.toLowerCase();
+		return "_" + y.toLowerCase();
 	}).replace(/^_/, "");
